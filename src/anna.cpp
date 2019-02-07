93e991d537a234ba5e8932b3a6019e052cec3fc7
Michał <michal@garapich.pl>
2019-02-07 17:45:42 +0100

    feat: add cors & crsf protection

--- src/anna.cpp
+++ src/anna.cpp
@@ -1,13 +1,16 @@
 #include "anna.h"
 #include "daemon.h"
 #include "servers.h"
+#include <Cutelyst/Plugins/CSRFProtection/CSRFProtection>
+#include <QtCore>
 
 using namespace Cutelyst;
 
 Anna::Anna(QObject* parent) :
     Application(parent)
 {
-
+    // cors
+    defaultHeaders().setHeader("Access-Control-Allow-Origin", "*");
 }
 
 Anna::~Anna()
@@ -17,8 +20,13 @@ Anna::~Anna()
 
 bool Anna::init()
 {
+    // endpoints
     new Daemon(this);
     new Servers(this);
+
+    // plugins
+    new CSRFProtection(this);
+
     return true;
 }
 
